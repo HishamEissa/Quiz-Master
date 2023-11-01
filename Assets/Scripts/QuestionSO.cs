b2740ccf5dc84d68234f9801b76a8029a6db0292
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-01 19:55:49 +0200

    Added a getter function to the scriptable object

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,4 +7,9 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question ="Enter new Question text here";
+
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
