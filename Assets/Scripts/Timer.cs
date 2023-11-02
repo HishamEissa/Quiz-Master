64f272638ea344a53dd0c0f9406ae04b6c180435
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-02 12:24:31 +0200

    Added code that would reflect the remaining time on the counter sprite

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,7 +7,9 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
     float timerValue;
 
 
@@ -16,22 +18,45 @@ public class Timer : MonoBehaviour
     {
         UpdateTimer();
     }
+
+    public void CancelTimer()
+    {
+        timerValue = 0f;
+    }
+
+
     
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
-        if(timerValue <= 0 && isAnsweringQuestion)
+        if(isAnsweringQuestion)
         {
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                timerValue = timeToShowCorrectAnswer;
+                isAnsweringQuestion = false;
+            }
             
-            timerValue = timeToShowCorrectAnswer;
-            isAnsweringQuestion = false;
         }
-        else if (timerValue <= 0)
+        else 
         {
-            timerValue = timeToCompleteQuestion;
-            isAnsweringQuestion = true;
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = true;
+                loadNextQuestion = true;
+            }
+            
         }
-        Debug.Log(timerValue);
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + ": " + fillFraction);
     }
 }
