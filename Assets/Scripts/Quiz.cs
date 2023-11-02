33c074c7628d4a18f0eda89fe9362011bc8f8e4b
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-02 17:13:30 +0200

    Added a widget to display the score to the player
    Its fully coded and functional

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -27,9 +27,14 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
     void Start()
     {
-        timer = FindObjectOfType<Timer>();    
+        timer = FindObjectOfType<Timer>();  
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
     
     void Update()
@@ -55,6 +60,7 @@ public class Quiz : MonoBehaviour
 
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -66,6 +72,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -85,6 +92,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
         
     }
